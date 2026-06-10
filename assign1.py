07c1c17887e99608a50efe3c2589796e6d2ebdb2
ajitesh0102 <ajiteshpandey0102@gmail.com>
2026-06-10 16:31:06 +0530

    Initial commit

---
+++ assign1.py
@@ -0,0 +1,54 @@
+# list print kro 
+
+# import numpy as np 
+# arr=np.array([1,2,3])
+# print(arr)
+
+
+# matrix banao 
+# import numpy as np
+# arr=np.array([[1,2,3],[2,3,4],[4,5,6]])
+# print(arr)
+
+
+# zeroes banao 
+# import numpy as np
+# arr=np.zeros(3)
+# print(arr)
+
+
+# 2 * 4 zero matrix 
+# import numpy as np 
+# arr=np.zeros((2,4))
+# print(arr)
+
+
+
+# array of ones 
+# import numpy as np 
+# arr=np.ones(7)
+# print(arr)
+
+
+# full method use kro 
+# import numpy as np
+# arr=np.full([3,3],7)
+# print(arr)
+
+
+# arrange 
+# import numpy as np
+# arr=np.arange(0,20,2)
+# print(arr)
+
+
+# linspace 
+# import numpy as np 
+# arr=np.linspace(0,1,6)
+# print(arr)
+
+
+# random matrix
+# import numpy as np  
+# arr=np.random.randint(1,20,(3,3))
+# print(arr)
